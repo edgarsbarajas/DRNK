2395e0c601b1e8c1a28cce35516742097a678b77
Edgar Barajas <edgarsbarajas@yahoo.com>
2019-04-02 18:26:30 -0700

    Render a custom search svg in place

--- src/Router.js
+++ src/Router.js
@@ -1,58 +1,37 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, TouchableOpacity } from 'react-native';
 import { connect } from 'react-redux';
 import { Router , Scene } from 'react-native-router-flux';
 import SvgUri from 'react-native-svg-uri';
 import EventList from './components/EventList';
 import EventDetails from './components/EventDetails';
-import EventsNavBar from './components/EventsNavBar';
 import { toggleSearchBar } from './actions/SearchBarActions';
 
-class RouterComponent extends Component {
-  renderRightButton() {
-    return (
-      <View style={{ borderWidth: 1, borderColor: 'pink', flexDirection: 'row', justifyContent: 'flex-start', flex: 1, width: '100%' }}>
-      </View>
-    );
-  }
-
-  render() {
-    return (
-      <Router navigationBarStyle={{ backgroundColor: '#320086', borderBottomWidth: 0 }}>
-        <Scene key='root'>
-          <Scene
-            initial
-            key='eventsList'
-            component={EventList}
-            renderRightButton={this.renderRightButton}
-            renderTitle={<SvgUri height='75' width='75' source={require('../assets/svgs/logo.svg')} />}
-            onRight={this.props.toggleSearchBar}
-          />
-          <Scene
-            key='eventDetails'
-            component={EventDetails}
-            title='Details'
-            tintColor='#FFFFFF'
-            leftButtonTextStyle={{ color: '#FFFFFF' }}
-          />
-        </Scene>
-      </Router>
-    );
-  }
+const RouterComponent = props => {
+  return (
+    <Router navigationBarStyle={{ backgroundColor: '#320086', borderBottomWidth: 0 }}>
+      <Scene key='root'>
+        <Scene
+          initial
+          key='eventsList'
+          component={EventList}
+          renderRightButton={(
+            <TouchableOpacity onPress={props.toggleSearchBar}>
+              <SvgUri height='18' width='18' style={{ marginRight: 15 }} source={require('../assets/svgs/search.svg')} />
+            </TouchableOpacity>
+          )}
+          renderTitle={<SvgUri height='75' width='75' source={require('../assets/svgs/logo.svg')} />}
+        />
+        <Scene
+          key='eventDetails'
+          component={EventDetails}
+          title='Details'
+          tintColor='#FFFFFF'
+          leftButtonTextStyle={{ color: '#FFFFFF' }}
+        />
+      </Scene>
+    </Router>
+  );
 }
 
 export default connect(null, { toggleSearchBar })(RouterComponent);
-
-// navigationBarStyle={{ backgroundColor: '#320086', borderBottomWidth: 0 }}
-
-
-// <Scene
-//   initial
-//   key='eventsList'
-//   component={EventList}
-//   title='DRNK'
-//   renderRightButton={()=>filterIcon}
-//   onRight={this.props.toggleSearchBar}
-//   rightButtonImage={require('../assets/images/search.png')}
-//   titleStyle={{ color: '#FFFFFF', fontSize: 26, letterSpacing: -3.5, fontWeight: 'bold' }}
-// />
